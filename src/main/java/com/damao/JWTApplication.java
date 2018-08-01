6fb92307e074fecd9ba61d22014fba57bf47ca23
老虎 <huyongxing0811@163.com>
2018-08-01 17:54:58 +0800

    初始化

---
+++ src/main/java/com/damao/JWTApplication.java
@@ -0,0 +1,17 @@
+package com.damao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author huyongxing
+ * @email huyongxing@qihaiyun.com
+ * @date 2018/8/1 17:36
+ */
+@SpringBootApplication
+public class JWTApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(JWTApplication.class);
+
+    }
+}
